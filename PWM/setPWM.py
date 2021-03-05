5b5fddb87c8c5b35cbb99cd5840bce2fbb96310a
Wanarut B <wanarut.b@gmail.com>
2021-03-05 18:08:37 +0700

    test

--- PWM/setPWM.py
+++ PWM/setPWM.py
@@ -1,5 +1,4 @@
 import RPi.GPIO as GPIO
-from time import sleep
 import sys
 
 pwmPin = int(sys.argv[1])	    # PWM output pin
@@ -8,6 +7,9 @@ duty_cycle = int(sys.argv[2])   # Duty cycle value
 GPIO.setwarnings(False)			#disable warnings
 GPIO.setmode(GPIO.BCM)		#set pin numbering system
 GPIO.setup(pwmPin, GPIO.OUT)
+
 pi_pwm = GPIO.PWM(pwmPin, 1000) #create PWM instance with frequency
-pi_pwm.start(duty_cycle)	    #start PWM of required Duty Cycle
+pi_pwm.stop()
+pi_pwm.start(0)
+pi_pwm.ChangeDutyCycle(duty_cycle)	    #start PWM of required Duty Cycle
 print("PWM Done!")
